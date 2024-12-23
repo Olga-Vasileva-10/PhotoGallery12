6865d407b93f7e5b22fdd694be9918ed520c7538
Olga <Vasileva.o.p.2.0.0@gmail.com>
2024-12-23 20:14:11 +0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.photogallery
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PhotoGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //А менюшка то нужна была :(
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_photo_gallery)
+        val isFragmentContainerEmpty =
+            savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent
+        {
+            return Intent(context,
+                PhotoGalleryActivity::class.java)
+        }
+    }
+
+}
